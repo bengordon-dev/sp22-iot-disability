19c2463bf4fae3394691480af77f986000d6e2b5
Ben Gordon <benjamintheepic@gmail.com>
2022-03-20 17:52:50 -0400

    spring break testing of distance sensor and accelerometer

---
+++ arduino-scripts/two_sensors_test/two_sensors_test.ino
@@ -0,0 +1,79 @@
+#include "Wire.h" // This library allows you to communicate with I2C devices.
+#include <math.h>
+#include <NewPing.h>
+
+#define TRIGGER_PIN  3
+#define ECHO_PIN     2
+#define MAX_DISTANCE 400 // Maximum distance we want to measure (in centimeters).
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
+int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
+int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
+int16_t temperature; // variables for temperature data
+char tmp_str[7]; // temporary variable used in convert function
+
+char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
+}
+
+double acc_x, acc_y, acc_z;
+double angle;
+
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
+  Wire.write(0x6B); // PWR_MGMT_1 register
+  Wire.write(0); // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+}
+void loop() {
+  delay(50);
+  
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
+  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
+  Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
+  
+  // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
+  accelerometer_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+  accelerometer_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+  accelerometer_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
+  temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
+  gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
+  gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
+  gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+
+  acc_x = (double) accelerometer_x;
+  acc_y = (double) accelerometer_y;
+  acc_z = (double) accelerometer_z;
+  
+  
+  // print out data
+  Serial.print("aX = "); Serial.print(convert_int16_to_str(accelerometer_x));
+  Serial.print(" | aY = "); Serial.print(convert_int16_to_str(accelerometer_y));
+  Serial.print(" | aZ = "); Serial.print(convert_int16_to_str(accelerometer_z));
+
+
+  Serial.print(" | Hypot (y, z) = "); Serial.print(hypot(acc_y, acc_z));
+  angle = atan2(hypot(acc_y, acc_z), acc_x)*180/M_PI;
+  Serial.print(" | Angle: = "); Serial.print(angle);
+  Serial.println();
+
+  if (fabs(angle) <= 15.0) {
+    int distance = sonar.ping_cm();   
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println("cm");
+  } else {
+      Serial.println("Angle too high");
+  }
+  
+  
+  // delay
+  //delay(1000);
+}
